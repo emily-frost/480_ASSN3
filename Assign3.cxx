7a9f973bae3a8502ff915fabca5187199ffd5f2a
sampiecz <samuel.piecz@gmail.com>
2019-09-29 05:38:54 -0500

    Still far off. Pipe command in process.

--- Assign3.cxx
+++ Assign3.cxx
@@ -14,90 +14,109 @@
 #include <vector>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
 
-using std::cout;
-using std::endl;
-using std::cin;
-using std::string;
-using std::vector;
+using namespace std;
 
-void doPipe()
+void doPipe(char *buf)
 {
-  cout << "It's a pipe." << endl;
-}
+  char * ar1[32], * ar2[32];
+  char * p, * p2;
+  int counter = 0;
+  int pipe1[2], pipe2[2];
+  bool firstPipe = true;
 
-string getInput(string command)
-{
-  cout << "480shell>";
-  getline(cin, command);
-  return command;
-}
-
-void executeCommand(string command)
-{
-	string realCommand, token, delimiter = " ";
-	vector<string> buffer; 
-	size_t pos = 0;
-	int count = 0;
-
-	while ((pos = command.find(delimiter)) != std::string::npos)
-	{
-		token = command.substr(0, pos);
-		if ( count == 0 )
-		{
-			realCommand = token;
-		}
-		buffer.push_back(token);
-		command.erase(0, pos + delimiter.length());
-		cout << token << endl;
-		count++;
-	}
-
-	char * args[buffer.size()];
-	count = 0;
-	for ( auto x : buffer )
-	{
-		char * writable = new char[x.size() + 1];
-		copy(x.begin(), x.end(), writable);
-		writable[x.size()] = '\0'; 
-		args[count] = writable;
-		delete[] writable;
-		count++;
-	}
-	args[count] = (char *) NULL;
-	
-	execvp(realCommand.c_str(), args);
-
-}
-
-int main()
-{
-
-  // Cool bool
-  bool cool = true;
-
-  // We cool?
-  while(cool)
+  p = strtok(buf, "||");
+  while (p != NULL)
   {
-    string command;
-    command = getInput(command);
-
-    // Should we stop?
-    if(command.find("quit") != string::npos || command.find("q") != string::npos)
+    if( firstPipe == true)
     {
-      cool = false;
+      p2 = strtok(p, " ");
+      while (p2 != NULL)
+      {
+        ar1[counter] = p2;     
+        p2 = strtok(NULL, " ");
+        counter++;
+      }
+      firstPipe = false;
+      counter = 0;
     }
-    // If not stopping, see if pipe
-    else if (command.find("||") != string::npos)
-    {
-      doPipe();
-    }
-    // If no pipe and no stop just do the command
     else
     {
-      executeCommand(command);
+      p2 = strtok(p, " ");
+      while (p2 != NULL)
+      {
+        ar2[counter] = p2;     
+        p2 = strtok(NULL, " ");
+        counter++;
+      }
+      counter = 0;
     }
-  } 
+  }
+  cout << "ar1" << endl;
+  for ( auto x : ar1 )
+    cout << x << endl;
+
+}
+
+int main(void)
+{
+  bool cool = true;
+
+  while (cool)
+  {
+    char buf[1024];
+    printf("480shell> ");
+    string command; 
+    cin.getline(buf, sizeof(buf));
+    int status;
+    char * args[32];
+    char *p;
+    pid_t pid;
+    int count = 0;
+    p = strtok(buf, " ");
+    while (p != NULL)
+    {
+      if (strcmp(p, "quit") == 0 || strcmp(p, "q") == 0)
+      {
+        cool = false;
+      }
+      else if (strcmp(p, "||") == 0)
+      {
+        doPipe(buf);
+      }
+      else if ( count == 0 )
+      {
+        command = p;
+        args[count] = p;
+        count++;
+      }
+      else
+      {
+        args[count] = p;
+        count++;
+      }
+      p = strtok(NULL, " ");
+    }
+    args[count] = (char *) NULL;
+
+    if ( (pid = fork()) < 0 )
+    {
+			printf("fork error");
+    }
+    else if ( pid == 0 )
+    {
+      execvp(command.c_str(), args);
+    }
+    if ( (pid = waitpid(pid, &status, 0)) < 0)
+    {
+			printf("waitpid error");  
+      cool = false;
+    }
+  }
 
   return 0;
-}
+
+} 
